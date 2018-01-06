070f11abe2970dd42e26a51dd13cda156d7fc368
Stephen Park <ssp0929@gmail.com>
2018-01-05 20:00:23 -0600

    Selling should work now. Although, I would need to put in like $10 to test it lol.

--- cryptopia_script.py
+++ cryptopia_script.py
@@ -7,12 +7,8 @@ Script sells at market rate, withdraws to wallet.
 # pylint: disable=C0301, C0103, R0914, E0401
 
 import time
-import random
-import hmac
-import hashlib
-import base64
-import json
 import requests
+from cryptopia_api import Api
 
 def main():
 
@@ -44,150 +40,69 @@ def main():
 
     # ----------------Don't change values below----------------------
 
+    # Initialize API with key and secret values
+    api = Api(API_KEY, API_SECRET)
+
     # Balance check loop
     script_running = True
-    balance = []
     while script_running:
-        # Reset variables
-        do_sell = False
-        do_withdraw = False
-
-        # Get relevent balances.
-        balance = check_exchange_balance(API_KEY, API_SECRET, SELL_CURRENCY, WITHDRAW_CURRENCY)
-        time.sleep(3)
+        market = SELL_CURRENCY + '/BTC'
 
         # Check if balances meet requirements to sell/withdraw
-        if balance[0] > SELL_THRESHOLD:
-            do_sell = True
-        if balance[1] > WITHDRAW_THRESHOLD:
-            do_withdraw = True
+        print '\nPreparing to check balance of ' + SELL_CURRENCY + ' you have deposited to the exchange...\n'
+        balance, error = api.get_balance(SELL_CURRENCY)
+        print '\nYou have a balance of ' + str(balance['Available']) + ' ' + SELL_CURRENCY + '.\n'
+        if error:
+            print '\nError getting balance for ' + SELL_CURRENCY + '.'
+            print 'Error message: ' + error + '.\n'
+        elif balance['Available'] > SELL_THRESHOLD:
+            # Request LTC/BTC market data
+            url = 'https://www.cryptopia.co.nz/api/GetMarkets/BTC'
+            response = requests.get(url).json()
+            data = response['Data']
 
-        # DEBUG
-        market_rate_sell(API_KEY, API_SECRET, SELL_CURRENCY, balance[0])
-        withdraw_to_address(API_KEY, API_SECRET, WALLET_ADDRESS, WITHDRAW_CURRENCY, balance[1])
+            # Find LTC/BTC row
+            for item in data:
+                if item['Label'] == 'LTC/BTC':
+                    rate = item['AskPrice']
+                    break
 
-        # Sell and/or withdraw if requirements met.
-        if do_sell:
-            # Sell at market rate
-            market_rate_sell(API_KEY, API_SECRET, SELL_CURRENCY, balance[0])
-            time.sleep(3)
-        elif do_withdraw:
-            # Withdraw to wallet
-            withdraw_to_address(API_KEY, API_SECRET, WALLET_ADDRESS, WITHDRAW_CURRENCY, balance[1])
+            # Check if minimum trading limit is met
+            min_check = '{0:f}'.format(rate*balance['Available'])
+            min_to_meet_limit = '{0:f}'.format((rate*balance['Available'])/.0005)
+            print '\nPreparing to check if minimum trading limit is met...\n'
+            if rate*balance['Available'] < .0005:
+                print '\nSorry, minimum trading limit is the equivalent of .0005 BTC.'
+                print 'You are currently attempting to trade the equivalent of ' + str(min_check) + ' BTC.'
+                print 'You would need to trade at least ' + str(min_to_meet_limit) + ' ' + SELL_CURRENCY + '.\n'
+            else:
+                # Create sell order.
+                print '\nMinimum trading limit met.'
+                print '\nCreating sell order for ' + str(balance['Available']) + \
+                    ' ' + SELL_CURRENCY + ' at market asking price of ' + \
+                    str(rate) + ' BTC to 1 ' + SELL_CURRENCY + '.\n'
+                result, error = api.submit_trade(market, 'Sell', rate, balance['Available'])
+                if error:
+                    print '\nError while attempting to create sell order.'
+                    print 'Error message: ' + error + '.\n'
+                else:
+                    print '\nSell successful.'
+                    print 'Response received: ' + result + '.\n'
+
+                # DEBUG exit after submitting trade
+                break
+
+        # Slow script for now for debugging purposes.
+        time.sleep(100)
+
+        balance, error = api.get_balance(WITHDRAW_CURRENCY)
+        if error:
+            print 'Error getting balance for ' + WITHDRAW_CURRENCY + '.'
+        if balance > WITHDRAW_THRESHOLD:
+            api.submit_withdraw(WITHDRAW_CURRENCY, WALLET_ADDRESS, amount)
 
         # Sleep for allotted time.
         time.sleep(SECONDS_TO_SLEEP)
-
-def generate_header(API_KEY, API_SECRET, url, post_data):
-
-    ''' Generate secure header for private API calls '''
-
-    # Generate a Request Time Stamp and use as a nonce, UNIQUE
-    nonce = str(int(time.time())) + '.' + str(random.randint(0, 1000))
-
-    # Generate signature data (API_KEY/SECRET, RequestURL+Method, Nonce, base64string)
-    md5 = hashlib.md5()
-    md5.update(post_data.encode('UTF-8'))
-    b64 = base64.b64encode(md5.digest())
-    request_signature = API_KEY + 'POST' + url + nonce + b64.decode("UTF-8")
-
-    # Sign using HMAC-SHA256
-    hmacraw = hmac.new(API_SECRET, request_signature, hashlib.sha256).digest()
-    sign = base64.b64encode(hmacraw)
-
-    # Combine into request header
-    request_header = 'amx ' + API_KEY + ':' + sign + ':' + nonce
-    print request_header + '\n'
-
-    return {'Authorization': request_header, 'Content-Type': 'application/json; charset=utf-8'}
-
-def check_exchange_balance(API_KEY, API_SECRET, SELL_CURRENCY, WITHDRAW_CURRENCY, req=None):
-
-    ''' Check balance of COIN_SYMBOL and BTC in exchange '''
-
-    # Generate header and send POST request
-    url = 'https://www.cryptopia.co.nz/Api/GetBalance'
-    post_data = json.dumps(req)
-    headers = generate_header(API_KEY, API_SECRET, url, post_data)
-    response = requests.post(url, data=post_data, headers=headers)
-    response.encoding = 'utf-8-sig'
-
-    # Grab balances from response
-    data = json.loads(response.text)
-    print data
-    data = data['Data']
-
-    for currency in data:
-        if data['Symbol'] == SELL_CURRENCY:
-            sell_balance = currency['Available']
-        if data['Symbol'] == WITHDRAW_CURRENCY:
-            withdraw_balance = currency['Available']
-
-    return [sell_balance, withdraw_balance]
-
-def withdraw_to_address(API_KEY, API_SECRET, WALLET_ADDRESS, WITHDRAW_CURRENCY, withdraw_amount):
-
-    ''' Withdraw to wallet address given '''
-
-    # Build request object
-    req = {
-        'Currency': WITHDRAW_CURRENCY,
-        'Address': WALLET_ADDRESS,
-        'Amount': withdraw_amount
-    }
-
-    # POST REQUEST URL
-    url = 'https://www.cryptopia.co.nz/Api/SubmitWithdraw'
-
-    # Generate request signature and send post request
-    nonce = str(int(time.time()))
-    post_data = json.dumps(req)
-    m = hashlib.md5()
-    m.update(post_data.encode('utf-8'))
-    requestContentBase64String = str(base64.b64encode(m.digest()))
-    signature = API_KEY + 'POST' + url + nonce + requestContentBase64String
-    hmacsignature = str(base64.b64encode(hmac.new(base64.b64decode(API_SECRET), signature.encode('utf-8'), hashlib.sha256).digest()))
-    header_value = 'amx ' + API_KEY + ':' + hmacsignature + ':' + nonce
-    headers = {'Authorization': header_value, 'Content-Type':'application/json; charset=utf-8'}
-    response = requests.post(url, data=post_data, headers=headers)
-
-    # Print response to terminal.
-    print('\nAPI call response:\n\n', response.text)
-
-def market_rate_sell(API_KEY, API_SECRET, SELL_CURRENCY, amount):
-
-    ''' Sell coin at market rate. '''
-
-    # Get market rate of SELL_CURRENCY/BTC asking price.
-    response = requests.get('https://www.cryptopia.co.nz/api/GetMarket/LTC_BTC')
-    data = json.loads(response.text)
-
-    # Build request object
-    req = {
-        'Market': SELL_CURRENCY + '/BTC',
-        'Type': 'Sell',
-        'Rate': data,
-        'Amount': amount
-    }
-
-    # POST REQUEST URL
-    url = 'https://www.cryptopia.co.nz/Api/SubmitTrade'
-
-    # Generate request signature and send post request
-    nonce = str(int(time.time()))
-    post_data = json.dumps(req)
-    m = hashlib.md5()
-    m.update(post_data.encode('utf-8'))
-    requestContentBase64String = str(base64.b64encode(m.digest()))
-    signature = API_KEY + 'POST' + url + nonce + requestContentBase64String
-    hmacsignature = str(base64.b64encode(hmac.new(base64.b64decode(API_SECRET), signature.encode('utf-8'), hashlib.sha256).digest()))
-    header_value = 'amx ' + API_KEY + ':' + hmacsignature + ':' + nonce
-    headers = {'Authorization': header_value, 'Content-Type':'application/json; charset=utf-8'}
-    response = requests.post(url, data=post_data, headers=headers)
-
-    # Print response to terminal.
-    print('\nAPI call response:\n\n', response.text)
-
 
 if __name__ == '__main__':
     main()
